3aeb389ae6270ac11579bd155892ba90a8e2c992
Hallam Smith <me@hcssmith.com>
2020-11-29 00:43:28 +0000

    added formating

--- config.h
+++ config.h
@@ -63,9 +63,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_state, "%s",           "BAT0" },
+	{ battery_state, "BAT: [%s",           "BAT0" },
 	{ battery_perc, "%s%%",           "BAT0" },
-	{ battery_remaining, " %s ",           "BAT0" },
-	{ wifi_essid, "Wifi: %s ",         "wlp59s0" },
-	{ datetime, "%s",           "%F %T" },
+	{ battery_remaining, " %s] ",           "BAT0" },
+	{ wifi_essid, "[Wifi: %s] ",         "wlp59s0" },
+	{ datetime, "%s",           "[%F %T]" },
 };
