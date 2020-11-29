b679f62bfff723fef74cba8ca830a594263e1e96
Hallam Smith <me@hcssmith.com>
2020-11-29 00:40:18 +0000

    added spacing

--- config.h
+++ config.h
@@ -63,9 +63,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_perc, "%s",           "BAT0" },
 	{ battery_state, "%s",           "BAT0" },
-	{ battery_remaining, "%s",           "BAT0" },
-	{ wifi_essid, "%s",         "wlp59s0" },
+	{ battery_perc, "%s%%",           "BAT0" },
+	{ battery_remaining, " %s ",           "BAT0" },
+	{ wifi_essid, "Wifi: %s ",         "wlp59s0" },
 	{ datetime, "%s",           "%F %T" },
 };
